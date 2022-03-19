1ad75ef39eb8e9ed962218e16db7a555e2be1d97
henmasta <n.semenuta4830@gmail.com>
2022-03-19 20:51:08 +0300

    abouts

--- src/main/resources/templates/script.js
+++ src/main/resources/templates/script.js
@@ -1,9 +1,9 @@
 function onEntry(entry) {
-  entry.forEach(change => {
-    if (change.isIntersecting) {
-     change.target.classList.add('element-show');
-    }
-  });
+    entry.forEach(change => {
+        if (change.isIntersecting) {
+            change.target.classList.add('element-show');
+        }
+    });
 }
 
 let options = { threshold: [0.5] };
@@ -11,5 +11,23 @@ let observer = new IntersectionObserver(onEntry, options);
 let elements = document.querySelectorAll('.blog__item');
 
 for (let elm of elements) {
-  observer.observe(elm);
+    observer.observe(elm);
+}
+
+function DropButton() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
 }
