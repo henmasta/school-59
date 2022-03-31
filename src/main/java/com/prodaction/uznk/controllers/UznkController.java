6079a035dede7fea467037ac4c218e8f6f132c21
henmasta <n.semenuta4830@gmail.com>
2022-03-31 16:36:01 +0300

    about

--- src/main/java/com/prodaction/uznk/controllers/UznkController.java
+++ src/main/java/com/prodaction/uznk/controllers/UznkController.java
@@ -29,7 +29,7 @@ public class UznkController {
 	@GetMapping("/abouts")
 	public String abouts(Model model) {
 		model.addAttribute("title", "abouts");
-		return "contact";
+		return "aboutus";
 	} 
 
 	@GetMapping("/items")
